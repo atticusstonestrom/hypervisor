10f58ff499a850df3607b8256ea70f0ce3395195
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-22 18:46:31 -0400

    Update and rename dev.c to hvc.c

---
+++ hvc.c
@@ -0,0 +1,96 @@
+//////////////////////////////////////////////////////
+//                                                  //
+//                                                  //
+//                                                  //
+//////////////////////////////////////////////////////
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+
+#define DEV_NAME "hvchar"
+#define CLASS_NAME "hvc"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Atticus Stonestrom");
+MODULE_DESCRIPTION("...");
+MODULE_VERSION("0.01");
+
+static int major_num;
+static char message[256]={0};
+static short size_of_message;
+static int counter=0;
+static struct class *hvc_class=NULL;
+static struct device *hvc_device=NULL;
+
+static int dev_open(struct inode *, struct file *);
+static int dev_release(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+
+static struct file_operations fops = {
+	.open=dev_open,
+	.read=dev_read,
+	.write=dev_write,
+	.release=dev_release };
+
+static int __init hvc_init(void) {
+	printk("[*] initializing the hvchar lkm\n");
+	if( (major_num=register_chrdev(0, DEVICE_NAME, &fops))<0 ) {
+		printk("[*] failed to register a major number\n");
+		return major_num; }
+	printk("[*] registered correctly with major number %d\n", major_num);
+	
+	hvc_class=class_create(THIS_MODULE, CLASS_NAME);
+	if(IS_ERR(hvf_class)) {
+		unregister_chrdev(major_num, DEVICE_NAME);
+		printk("[*] failed to register device class\n");
+		return PTR_ERR(hvc_class); }
+	printk("[*] device class registered correctly\n");
+	
+	hvc_device=device_create(hvc_class, NULL, MKDEV(major_num, 0), NULL, DEVICE_NAME);
+	if(IS_ERR(hvc_device)) {
+		class_destroy(hvc_class);
+		unregister_chrdev(major_num, DEVICE_NAME);
+		printk("[*] failed to create the device\n");
+		return PTR_ERR(hvc_device); }
+	printk("[*] device class created correctly\n");
+
+	return 0; }
+
+static void __exit hvc_exit(void) {
+	device_destroy(hvc_class, MKDEV(major_num, 0));
+	class_unregister(hvc_class);
+	class_destroy(hvc_class);
+	unregister_chrdev(major_num, DEVICE_NAME);
+	printk("[*] lkm unloaded\n"); }
+
+static int dev_open(struct inode *inodep, struct file *filep) {
+	printk("[*] device has been opened %d time(s)\n", ++counter);
+	return 0; }
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
+	int error=0;
+	if(!(error=copy_to_user(buffer, message, size_of_message))) {
+		printk("[*] sent %d characters to the user\n", size_of_message);
+		return (size_of_message=0); }
+	else {
+		printk("[*] failed to send %d characters to the user\n", error);
+		return -EFAULT; }}
+
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
+	len=(len<sizeof(message)) ? len:sizeof(message);
+	sprintf(message, "%s (%zu letters)", buffer, len);
+	size_of_message=strlen(message);
+	printk("[*] received %zu characters from the user", len);
+	return len; }
+
+static int dev_release(struct inode *inodep, struct file *filep) {
+	printk("[*] device succesfully closed\n");
+	return 0; }
+
+module_init(hvc_init);
+module_exit(hvc_exit);
