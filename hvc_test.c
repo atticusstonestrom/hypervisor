c989738d19a9784f3242dac14a2f80d689ac8501
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-22 18:57:01 -0400

    Update hvc_test.c

--- hvc_test.c
+++ hvc_test.c
@@ -5,7 +5,24 @@
 #include <string.h>
 #include <unistd.h>
 
-static char buffer[256];
+static char rx_buffer[256];
 
 int main() {
-	int ret, fd; }
+	int fd; 
+	char tx_buffer[256];
+	if( (fd=open("/dev/hvchar", O_RDWR))<0 ){
+		perror("fatal in open");
+		return errno; }
+	printf("type in a short string to send to the kernel module:\n");
+	scanf("%[^\n]%*c", tx_buffer);
+	printf("writing message to the device [%s]\n", tx_buffer);
+	if(write(fd, tx_buffer, strlen(tx_buffer))<0) {
+		perror("fatal in write");
+		return errno; }
+	printf("press enter to read back from the device\n");
+	getchar();
+	if(read(fd, rx_buffer, sizeof(rx_buffer))<0) {
+		perror("fatal in read");
+		return errno; }
+	printf("received: [%s]\n", rx_buffer);
+	return 0; }
