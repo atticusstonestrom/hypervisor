f8bf767b430ed9d59c5435a47d7bb8832bc9a5d1
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-22 18:48:25 -0400

    Create hvc_test.c

---
+++ hvc_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+static char buffer[256];
+
+int main() {
+	int ret, fd; }
