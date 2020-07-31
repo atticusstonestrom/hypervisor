a88e86411a87ae8db2a9dbaa5e5ae88ff49fb390
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-31 11:36:19 -0400

    Update hvc.c

--- hvc.c
+++ hvc.c
@@ -61,7 +61,6 @@ static struct file_operations fops = {
 /////////////////////////////////////////
 
 /////////////////////////////////////////
-//guest state variables
 typedef struct {
 	unsigned long vmxon_region;
 	unsigned long vmxon_paddr;
@@ -74,18 +73,79 @@ typedef struct {
 	
 	unsigned long msr_bitmap;
 	unsigned long msr_paddr;
-} vmstate_t;
-vmstate_t vmstate;
+	
+	int active_flag;
+} guest_state_t;
+guest_state_t guest_state;
+/////////////////////////////////////////
+typedef struct {
+	cr4_t old_cr4;
+	int vmxon_flag;
+	//linked list of active guest_states?
+} host_state_t;
+host_state_t host_state;
 /////////////////////////////////////////
 
-/////////////////////////////////////////
-//host state variables
-cr4_t initial_cr4;
-/////////////////////////////////////////
+void cleanup(guest_state_t *vm_state, host_state_t *vmm_state) {
+	printk("–––––––––––––––––––––––––––––––––––––––––––––––––––––\n\n");
+	
+	rflags_t rflags;
+	if(vm_state->active_flag && vm_state->vmcs_paddr) {
+		printk("[*]  clearing vmcs\n");
+		printk("[**] vmcs region:\t0x%lx\n", vm_state->vmcs_paddr);
+		__asm__ __volatile__(
+			"vmclear %1;"
+			"pushf;"
+			"popq %0;"
+			:"=r"(rflags.val)
+			:"m"(vm_state->vmcs_paddr)
+			:"memory");
+		printk("[**] rflags:\t0x%lx\n", rflags.val);
+		if(!VMsucceed(rflags)) {
+			if(VMfailValid(rflags)) {
+				//should get error field from current vmcs
+				printk("[*]  vmclear failed with valid region\n"); }
+			else if(VMfailInvalid(rflags)) {
+				printk("[*]  vmclear failed with invalid region\n"); }}
+		else {
+			vm_state->active_flag=0;
+			printk("[*]  vmclear succeeded\n\n"); }}
+		
+	if(vmm_state->vmxon_flag) {
+		printk("[*]  exiting vmx mode\n\n");
+		__asm__ __volatile__("vmxoff"); 
+		vmm_state->vmxon_flag=0; }
+	
+	cr4_t cr4;
+	if(vmm_state->old_cr4.val) {
+		printk("[*]  restoring initial cr4\n");
+		__asm__ __volatile__("mov %%cr4, %0":"=r"(cr4.val));
+		printk("[**] cr4:\t0x%lx\n", cr4.val);
+		cr4.val=vmm_state->old_cr4.val;
+		__asm__ __volatile__("mov %0, %%cr4"::"r"(cr4.val));
+		printk("[**] new cr4:\t0x%lx\n", cr4.val);
+		printk("[*]  restored\n\n");
+		vmm_state->old_cr4.val=0; }
+	
+	printk("[*]  freeing pages\n");
+	free_ept(&(vm_state->ept_data));
+	if(vm_state->vmxon_region) {
+		printk("[**] vmxon region:\t0x%lx\n", vm_state->vmxon_region); 
+		free_page(vm_state->vmxon_region);
+		vm_state->vmxon_region=0; }
+	if(vm_state->vmcs_region) {
+		printk("[**] vmcs region:\t0x%lx\n", vm_state->vmcs_region);
+		free_page(vm_state->vmcs_region);
+		vm_state->vmcs_region=0; }
+	printk("[*]  all freed\n\n"); }
+
 	
 
 static int __init hvc_init(void) {
 	//all of this should run only on a single processor
+	
+	guest_state=(guest_state_t) {0};
+	host_state=(host_state_t) {0};
 	
 	cpuid_t cpuid;
 	printk("[*]  verifying vt-x support\n");
@@ -105,7 +165,7 @@ static int __init hvc_init(void) {
 	printk("[*]  setting cr4.vmxe[bit 13]\n");
 	__asm__ __volatile__("mov %%cr4, %0":"=r"(cr4.val));
 	printk("[**] cr4:\t0x%lx\n", cr4.val);
-	initial_cr4.val=cr4.val;
+	host_state.old_cr4.val=cr4.val;
 	cr4.vmxe=1;
 	__asm__ __volatile__("mov %0, %%cr4"::"r"(cr4.val));
 	printk("[**] new cr4:\t0x%lx\n", cr4.val);
@@ -120,7 +180,7 @@ static int __init hvc_init(void) {
 		//should check if current processor state is smx
 		if(!msr.feature_control.non_smx_vmxe) {
 			printk("[*] non-smx vmx disabled\n");
-			__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
+			cleanup(&guest_state, &host_state);
 			return -EOPNOTSUPP; }
 		printk("[**] non-smx vmx enabled\n"); }
 	else {
@@ -138,28 +198,32 @@ static int __init hvc_init(void) {
 	       msr.pat.entries[0], msr.pat.entries[1], msr.pat.entries[2], msr.pat.entries[3], 
 	       msr.pat.entries[4], msr.pat.entries[5], msr.pat.entries[6], msr.pat.entries[7]);
 	if(msr.pat.entries[0]!=PAT_WB && msr.pat.entries[4]!=PAT_WB) {
-		printk("[*]  writeback caching not available\n");
-		__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
+		printk("[*]  writeback caching not available\n\n");
+		cleanup(&guest_state, &host_state);
 		return -EOPNOTSUPP; }
 	printk("[*]  writeback caching available\n\n");
 	
 	
 	int ret=0;
-	if(( ret=alloc_wb_page("vmxon region", &(vmstate.vmxon_region), &(vmstate.vmxon_paddr)) )) {
-		__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
+	if(( ret=alloc_wb_page("vmxon region", &(guest_state.vmxon_region), &(guest_state.vmxon_paddr)) )) {
+		cleanup(&guest_state, &host_state);
 		return ret; }
 
 
-	if(( ret=alloc_wb_page("vmcs region", &(vmstate.vmcs_region), &(vmstate.vmcs_paddr)) )) {
-		__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
-		free_page(vmstate.vmxon_region);
+	if(( ret=alloc_wb_page("vmcs region", &(guest_state.vmcs_region), &(guest_state.vmcs_paddr)) )) {
+		cleanup(&guest_state, &host_state);
 		return ret; }
+	
+	if( (ret=initialize_ept(&guest_state.ept_data, MAX_ORD_GUEST_PAGES)) ) {
+		cleanup(&guest_state, &host_state);
+		return ret; }
+	//free_ept(&guest_state.ept_data);
 	
 	
 	printk("[*]  entering vmx operation\n");
 	READ_MSR(msr, IA32_VMX_BASIC);
 	printk("[**] rev id:\t0x%x\n", msr.vmx_basic.revision_id);
-	*(unsigned int *)(vmstate.vmxon_region)=msr.vmx_basic.revision_id;
+	*(unsigned int *)(guest_state.vmxon_region)=msr.vmx_basic.revision_id;
 	rflags_t rflags;
 		//__asm__ __volatile__(
 		//	"vmxon %1;"
@@ -175,14 +239,14 @@ static int __init hvc_init(void) {
 		//	"popq %0;"
 		//
 		//	:"=r"(rflags.val)
-		//	:"m"(vmstate.vmxon_paddr)
+		//	:"m"(guest_state.vmxon_paddr)
 		//	:"memory");
 	__asm__ __volatile__(		//define this as macro
 		"vmxon %1;"
 		"pushf;"
 		"popq %0;"
 		:"=r"(rflags.val)
-		:"m"(vmstate.vmxon_paddr)
+		:"m"(guest_state.vmxon_paddr)
 		:"memory");
 	printk("[**] rflags:\t0x%lx\n", rflags.val);
 	if(!VMsucceed(rflags)) {
@@ -191,24 +255,23 @@ static int __init hvc_init(void) {
 			printk("[*]  vmxon failed with valid region\n"); }
 		else if(VMfailInvalid(rflags)) {
 			printk("[*]  vmxon failed with invalid region\n"); }
-		__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
-		free_page(vmstate.vmxon_region);
-		free_page(vmstate.vmcs_region);
+		cleanup(&guest_state, &host_state);
 		return -EINVAL; }
+	host_state.vmxon_flag=1;
 	printk("[*]  vmx operation entered\n\n"); 
 	
 
 	printk("[*]  activating vmcs region\n");
 	READ_MSR(msr, IA32_VMX_BASIC);
 	printk("[**] rev id:\t0x%x\n", msr.vmx_basic.revision_id);
-	*(unsigned int *)(vmstate.vmcs_region)=msr.vmx_basic.revision_id;
-	printk("[**] clearing vmcs @ 0x%lx\n", vmstate.vmcs_paddr);
+	*(unsigned int *)(guest_state.vmcs_region)=msr.vmx_basic.revision_id;
+	printk("[**] clearing vmcs @ 0x%lx\n", guest_state.vmcs_paddr);
 	__asm__ __volatile__(
 		"vmclear %1;"
 		"pushf;"
 		"popq %0;"
 		:"=r"(rflags.val)
-		:"m"(vmstate.vmcs_paddr)
+		:"m"(guest_state.vmcs_paddr)
 		:"memory");
 	printk("[**] rflags:\t0x%lx\n", rflags.val);
 	if(!VMsucceed(rflags)) {
@@ -217,10 +280,7 @@ static int __init hvc_init(void) {
 			printk("[*]  vmclear failed with valid region\n"); }
 		else if(VMfailInvalid(rflags)) {
 			printk("[*]  vmclear failed with invalid region\n"); }
-		__asm__ __volatile__("vmxoff");
-		__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
-		free_page(vmstate.vmxon_region);
-		free_page(vmstate.vmcs_region);
+		cleanup(&guest_state, &host_state);
 		return -EINVAL; }
 	printk("[**] vmclear successful\n"); 
 	printk("[**] calling vmptrld\n");
@@ -229,7 +289,7 @@ static int __init hvc_init(void) {
 		"pushf;"
 		"popq %0;"
 		:"=r"(rflags.val)
-		:"m"(vmstate.vmcs_paddr)
+		:"m"(guest_state.vmcs_paddr)
 		:"memory");
 	printk("[**] rflags:\t0x%lx\n", rflags.val);
 	if(!VMsucceed(rflags)) {
@@ -238,27 +298,13 @@ static int __init hvc_init(void) {
 			printk("[*]  vmptrld failed with valid region\n"); }
 		else if(VMfailInvalid(rflags)) {
 			printk("[*]  vmptrld failed with invalid region\n"); }
-		__asm__ __volatile__("vmxoff");
-		__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
-		free_page(vmstate.vmxon_region);
-		free_page(vmstate.vmcs_region);
+		cleanup(&guest_state, &host_state);
 		return -EINVAL; }
+	guest_state.active_flag=1;
 	printk("[*]  vmcs region activated\n\n"); 
 	
-	__asm__ __volatile__("vmclear %0;"::"m"(vmstate.vmcs_paddr):"memory");
-	
-	
-	
-
-	if( (ret=initialize_ept(&vmstate.ept_data, MAX_ORD_GUEST_PAGES)) ) {
-		__asm__ __volatile__("vmclear %0;"::"m"(vmstate.vmcs_paddr):"memory");
-		__asm__ __volatile__("vmxoff");
-		__asm__ __volatile__("mov %0, %%cr4"::"r"(initial_cr4.val));
-		free_page(vmstate.vmxon_region);
-		free_page(vmstate.vmcs_region);
-		return ret; }
-	free_ept(&vmstate.ept_data);
-	//if(eptp_list==NULL) {
+	__asm__ __volatile__("vmclear %0;"::"m"(guest_state.vmcs_paddr):"memory");
+	guest_state.active_flag=0;
 		
 	
 	
@@ -286,24 +332,7 @@ static int __init hvc_init(void) {
 	return 0; }
 
 static void __exit hvc_exit(void) {
-	printk("[*]  exiting vmx mode\n\n");
-	__asm__ __volatile__("vmxoff");
-	
-	cr4_t cr4;
-	printk("[*]  restoring initial cr4\n");
-	__asm__ __volatile__("mov %%cr4, %0":"=r"(cr4.val));
-	printk("[**] cr4:\t0x%lx\n", cr4.val);
-	cr4.val=initial_cr4.val;
-	__asm__ __volatile__("mov %0, %%cr4"::"r"(cr4.val));
-	printk("[**] new cr4:\t0x%lx\n", cr4.val);
-	printk("[*]  restored\n\n");
-	
-	printk("[*]  freeing vm regions\n");
-	free_page(vmstate.vmxon_region);
-	printk("[**] vmxon:\t0x%lx\n", vmstate.vmxon_region);
-	free_page(vmstate.vmcs_region);
-	printk("[**] vmcs:\t0x%lx\n", vmstate.vmcs_region);
-	printk("[*]  all pages freed\n\n");
+	cleanup(&guest_state, &host_state);
 	
 	device_destroy(hvc_class, MKDEV(major_num, 0));
 	class_unregister(hvc_class);
