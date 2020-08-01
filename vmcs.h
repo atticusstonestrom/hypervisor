8fd70f501fa4c8bd9544116a14cc41a33f427a31
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-08-01 16:27:25 -0400

    Update vmcs.h

--- vmcs.h
+++ vmcs.h
@@ -8,117 +8,72 @@
 #define __VMCS
 
 
-#define VMXON(paddr, lhf)	\
-__asm__ __volatile__(		\
-	"vmxon %1;"		\
-	"lahf;"			\
-	"shr $8, %%rax;"	\
-	"movb %%al, %0;"	\
-	:"=r"(lhf.val)		\
-	:"m"(paddr)		\
-	:"rax", "memory")
+typedef struct {
+	unsigned long cr0;
+	unsigned long cr3;
+	unsigned long cr4;
+	unsigned long dr7;
+	unsigned long rsp;
+	unsigned long rip;
+	unsigned long rflags;
 
-#define VMCLEAR(paddr, lhf)	\
-__asm__ __volatile__(		\
-	"vmclear %1;"		\
-	"lahf;"			\
-	"shr $8, %%rax;"	\
-	"movb %%al, %0;"	\
-	:"=r"(lhf.val)		\
-	:"m"(paddr)		\
-	:"rax", "memory")
+	unsigned short cs_selector;
+	unsigned long cs_base;
+	unsigned int cs_lim;
+	unsigned int cs_access_rights;
+	
+	unsigned short ss_selector;
+	unsigned long ss_base;
+	unsigned int ss_lim;
+	unsigned int ss_access_rights;
+	
+	unsigned short ds_selector;
+	unsigned long ds_base;
+	unsigned int ds_lim;
+	unsigned int ds_access_rights;
+	
+	unsigned short es_selector;
+	unsigned long es_base;
+	unsigned int es_lim;
+	unsigned int es_access_rights;
+	
+	unsigned short fs_selector;
+	unsigned long fs_base;
+	unsigned int fs_lim;
+	unsigned int fs_access_rights;
+	
+	unsigned short gs_selector;
+	unsigned long gs_base;
+	unsigned int gs_lim;
+	unsigned int gs_access_rights;
+	
+	unsigned short ldtr_selector;
+	unsigned long ldtr_base;
+	unsigned int ldtr_lim;
+	unsigned int ldtr_access_rights;
+	
+	unsigned short tr_selector;
+	unsigned long tr_base;
+	unsigned int tr_lim;
+	unsigned int tr_access_rights;
+	
+	gdtr_t gdtr;
+	idtr_t idtr;
+	
+	msr_t ia32_debugctl;
+	msr_t ia32_sysenter_cs;
+	msr_t ia32_sysenter_esp;
+	msr_t ia32_sysenter_eip;
+	msr_t ia32_perf_global_ctrl;
+	msr_t ia32_pat;
+	msr_t ia32_efer;
+	msr_t ia32_bndcfgs;
 
-#define VMPTRLD(paddr, lhf)	\
-__asm__ __volatile__(		\
-	"vmptrld %1;"		\
-	"lahf;"			\
-	"shr $8, %%rax;"	\
-	"movb %%al, %0;"	\
-	:"=r"(lhf.val)		\
-	:"m"(paddr)		\
-	:"rax", "memory")
+	unsigned int smbase;
 
-#define VMLAUNCH(lhf)		\
-__asm__ __volatile__(		\
-	"vmlaunch;"		\
-	"lahf;"			\
-	"shr $8, %%rax;"	\
-	"movb %%al, %0;"	\
-	:"=r"(lhf.val)		\
-	::"rax", "memory")
-
-#define VMRESUME(lhf)		\
-__asm__ __volatile__(		\
-	"vmresume;"		\
-	"lahf;"			\
-	"shr $8, %%rax;"	\
-	"movb %%al, %0;"	\
-	:"=r"(lhf.val)		\
-	::"rax", "memory")
-
-#define VMXOFF   __asm__ __volatile__("vmxoff")
-
-
-//lower half flags
-//much better performance
-//than pushf/popf
-typedef union __attribute__((packed)) {
-	struct __attribute__((packed)) {
-		unsigned char cf:1;
-		unsigned char rsv_1:1;
-		unsigned char pf:1;
-		unsigned char rsv_3:1;
-		unsigned char af:1;
-		unsigned char rsv_5:1;
-		unsigned char zf:1;
-		unsigned char sf:1; };
-	unsigned char val;
-} lhf_t;
-
-#define VMsucceed(lhf)		(!(lhf).cf && !(lhf).zf)
-#define VMfailInvalid(lhf)	((lhf).cf && !(lhf).zf)
-#define VMfailValid(lhf)	(!(lhf).cf && (lhf).zf)
-
-typedef union __attribute__((packed)) {
-	struct __attribute__((packed)) {
-		unsigned long caching_type:3;
-		unsigned long page_walk_length:3;	//minus 1
-		unsigned long accessed_dirty_control:1;
-		unsigned long rsv_7_11:5;
-		unsigned long pml4_addr:40;	//bits 12 to 51
-		unsigned long rsv_52_63:12; };
-	unsigned long val;
-} eptp_t;
-
-typedef union __attribute__((packed)) {
-	struct __attribute__((packed)) {
-		unsigned long r:1;
-		unsigned long w:1;
-		unsigned long x:1;
-		unsigned long caching_type:3;
-		unsigned long ignore_pat:1;
-		unsigned long page_size:1;
-		unsigned long accessed:1;
-		unsigned long dirty:1;
-		unsigned long ux:1;	//user-mode x access
-		unsigned long rsv_11:1;
-		unsigned long addr:40;
-		unsigned long rsv_52_62:11; 
-		unsigned long suppress_ve:1; };
-	struct __attribute__((packed)) {
-		unsigned long rsv_4kb_0_11:12;
-		unsigned long addr_4kb:40;	//bits 12 to 51
-		unsigned long rsv_4kb_52_63:12; };
-	struct __attribute__((packed)) {
-		unsigned long rsv_1gb_0_29:30;
-		unsigned long addr_1gb:22;	//bits 30 to 51
-		unsigned long rsv_1gb_52_63:12; };
-	struct __attribute__((packed)) {
-		unsigned long rsv_2mb_0_20:21;
-		unsigned long addr_2mb:31;	//bits 21 to 51
-		unsigned long rsv_2mb_52_63:12; };
-	unsigned long val;
-} epse_t;
+	////////////////////////////////////////////////
+	
+	
 
 
 #endif
