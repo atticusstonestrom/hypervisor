280766de6e0b3d199cff97172d15c6b1a0d41c12
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-08-06 00:15:27 -0400

    Update vmcs.h

--- vmcs.h
+++ vmcs.h
@@ -1328,7 +1328,40 @@ printk("[**]\tbase:\t0x%lx\n", base)
 	printk("[**] vmcs link:\t0x%lx", 0xffffffffffffffff);
 	EC_VMWRITE(0xffffffffffffffff, VMCS_LINK_PTR_F, lhf, error_code);
 
-	
+	READ_MSR(msr, IA32_DEBUGCTL);
+	EC_VMWRITE(msr.val, GUEST_IA32_DEBUGCTL_F, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_DEBUGCTL_F, lhf);
+	READ_MSR(msr, IA32_SYSENTER_CS);
+	EC_VMWRITE(msr.val, GUEST_IA32_SYSENTER_CS, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_SYSENTER_CS, lhf);
+	READ_MSR(msr, IA32_SYSENTER_ESP);
+	EC_VMWRITE(msr.val, GUEST_IA32_SYSENTER_ESP, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_SYSENTER_ESP, lhf);
+	READ_MSR(msr, IA32_SYSENTER_EIP);
+	EC_VMWRITE(msr.val, GUEST_IA32_SYSENTER_EIP, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_SYSENTER_EIP, lhf);
+	READ_MSR(msr, IA32_PERF_GLOBAL_CTRL);
+	EC_VMWRITE(msr.val, GUEST_IA32_PERF_GLOBAL_CTRL_F, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_PERF_GLOBAL_CTRL_F, lhf);
+	READ_MSR(msr, IA32_PAT);
+	EC_VMWRITE(msr.val, GUEST_IA32_PAT_F, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_PAT_F, lhf);
+	READ_MSR(msr, IA32_EFER);
+	EC_VMWRITE(msr.val, GUEST_IA32_EFER_F, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_EFER_F, lhf);
+	READ_MSR(msr, IA32_BNDCFGS);
+	EC_VMWRITE(msr.val, GUEST_IA32_BNDCFGS_F, lhf);
+	READ_MSR(msr, IA32_RTIT_CTL);
+	EC_VMWRITE(msr.val, GUEST_IA32_RTIT_CTL_F, lhf);
+	READ_MSR(msr, IA32_S_CET);
+	EC_VMWRITE(msr.val, GUEST_IA32_S_CET, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_S_CET, lhf);
+	READ_MSR(msr, IA32_INTERRUPT_SSP_TABLE_ADDR);
+	EC_VMWRITE(msr.val, GUEST_IA32_ISTA, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_ISTA, lhf);
+	READ_MSR(msr, IA32_PKRS);
+	EC_VMWRITE(msr.val, GUEST_IA32_PKRS_F, lhf);
+	EC_VMWRITE(msr.val, HOST_IA32_PKRS_F, lhf);
 	
 	printk("[*]  initialization complete\n\n");
 	
