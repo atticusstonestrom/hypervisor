e22908b6d9b7c02ea5074f96231724edfd1c4843
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-08-10 06:47:26 -0400

    Update vmcs.h

--- vmcs.h
+++ vmcs.h
@@ -1361,8 +1361,8 @@ printk("[**]\tbase:\t0x%lx\n", base)
 	/*READ_MSR(msr, IA32_PAT);
 	printk("[**]\tpat:\t0x%lx\n", msr.val);
 	EC_VMWRITE(msr.val, GUEST_IA32_PAT_F, lhf, error_code);
-	EC_VMWRITE(msr.val, HOST_IA32_PAT_F, lhf, error_code);
-	READ_MSR(msr, IA32_EFER);
+	EC_VMWRITE(msr.val, HOST_IA32_PAT_F, lhf, error_code);*/
+	/*READ_MSR(msr, IA32_EFER);
 	printk("[**]\tefer:\t0x%lx\n", msr.val);
 	EC_VMWRITE(msr.val, GUEST_IA32_EFER_F, lhf, error_code);
 	EC_VMWRITE(msr.val, HOST_IA32_EFER_F, lhf, error_code);*/
