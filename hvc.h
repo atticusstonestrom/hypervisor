8ff1d88be4896a123ba1bd5a0707215f80a65a7f
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-08-15 19:22:07 -0400

    Update hvc.h

--- hvc.h
+++ hvc.h
@@ -29,4 +29,23 @@ typedef struct {
 	int active_flag;
 } state_t;
 
+typedef struct __attribute__((packed)) {
+	unsigned long rax;
+	unsigned long rbx;
+	unsigned long rcx;
+	unsigned long rdx;
+	unsigned long rbp;
+	unsigned long rdi;
+	unsigned long rsi;
+	unsigned long r8;
+	unsigned long r9;
+	unsigned long r10;
+	unsigned long r11;
+	unsigned long r12;
+	unsigned long r13;
+	unsigned long r14;
+	unsigned long r15;
+	unsigned long rflags;
+} regs_t;
+
 #endif
