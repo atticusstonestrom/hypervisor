b25ab18c3c3df7c4eab53469db66172d60a66a0c
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-08-31 20:15:57 -0400

    Update mm.h

--- mm.h
+++ mm.h
@@ -326,8 +326,32 @@ static int initialize_ept(ept_data_t *data) {
 	
 	unsigned long base, top;
 	epse_p=(void *)data->pds.base;
+	
 	//ensure var takes priority over fixed
-	if(def_type.fe) {}
+	#define PARSE_FIXED_MTRR(msr_id, name, base, inc)
+	if(def_type.fe) {
+		READ_MSR(msr, msr_id);
+		gprint("name:\t%02x %02x %02x %02x %02x %02x %02x %02x",
+		       msr.mtrr_fixed.entries[0], msr.mtrr_fixed.entries[1], 
+		       msr.mtrr_fixed.entries[2], msr.mtrr_fixed.entries[3], 
+		       msr.mtrr_fixed.entries[4], msr.mtrr_fixed.entries[5], 
+		       msr.mtrr_fixed.entries[6], msr.mtrr_fixed.entries[7]);
+		for(i=0; i<8; i++) {
+			//gaps D:
+			int j;
+	
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX64K_00000, fix64k_00000, 0, 0x10000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX16K_80000, fix16k_80000, 0x80000, 0x4000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX16K_A0000, fix16k_a0000, 0xa0000, 0x4000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_C0000, fix4k_c0000, 0xc0000, 0x1000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_C8000, fix4k_c8000, 0xc8000, 0x1000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_D0000, fix4k_d0000, 0xd0000, 0x1000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_D8000, fix4k_d8000, 0xd8000, 0x1000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_E0000, fix4k_e0000, 0xe0000, 0x1000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_E8000, fix4k_e8000, 0xe8000, 0x1000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_F0000, fix4k_f0000, 0xf0000, 0x1000);
+	PARSE_FIXED_MTRR(IA32_MTRR_FIX4K_F8000, fix4k_f8000, 0xf8000, 0x1000);
+		
 	//not optimal but saves lines
 	for(i=0; def_type.e && i<vcnt; i++) {	//is this the right e?
 		READ_MSR(msr, IA32_MTRR_PHYSBASE(i));
